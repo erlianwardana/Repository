471d2193805ea2278dc37b81ccc93c502233bb2f
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-05-22 09:09:23 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("Hello, World!")
 st.write("Ini adalah aplikasi Streamlit sederhana.")
+
+st.set_page_config(page_title="Hello World App", layout="centered")
+st.sidebar.header("Dashboard")
