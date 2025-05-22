c2eb8ab312bb1a310a1501bbcd7e48eb9a8e1211
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-05-22 09:10:47 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd 
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
 
 st.set_page_config(page_title="Prediction")
 st.title("Prediction")
