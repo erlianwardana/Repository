44731a40d7fbadae2c6dd3afd85ff52c9c94f503
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-06-04 08:13:04 +0700

    Update 1_dashboard.py

--- pages/1_dashboard.py
+++ pages/1_dashboard.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Dashboard", page_icon="🦈")
 st.sidebar.header("Dashboard")
+st.title("DATA COVID19 INDONESIA")
+
+data = pd.read_csv("Data_COVID19_Indonesia.csv")
+st.write(data)
