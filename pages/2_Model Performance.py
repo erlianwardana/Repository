cf1c06c081d670cf5ed12941c077fd952ca1f101
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-06-04 08:03:42 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import class(fication_report,accuracy_score)
 
 st.set_page_config(page_title="Model Performance", page_icon="🦈")
 st.sidebar.header("Model Performance")
 
-df = pd.read_csv("model/Data_COVID19_Indonesia.csv")
-dataset = pd.read_csv("https://github.com/erlianwardana/Repository/blob/main/Data_COVID19_Indonesia.csv")
+data = pd.read_csv("Data_COVID19_Indonesia.csv")
+st.write(data)
+
 
 
