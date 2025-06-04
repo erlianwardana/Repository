ea53522b5e099b4afb9450468edd68215131ea6d
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-06-04 07:46:22 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import class(fication_report,accuracy_score)
 
 st.set_page_config(page_title="Model Performance", page_icon="🦈")
 st.sidebar.header("Model Performance")
+
+df = pd.read_csv("dir/file.csv")
+
+@st.cache_data
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+
+csv = convert_df(df)
+
+st.download_button(
+   "Press to Download",
+   csv,
+   "file.csv",
+   "text/csv",
+   key='download-csv'
+)
