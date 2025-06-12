20239171c3eebeb39754c30bd9c440d682b95183
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-06-12 11:52:09 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -8,12 +8,6 @@ features = ['category', 'sub_category', 'segment', 'region', 'ship_mode',
             'price', 'quantity', 'sales', 'profit', 'payment_mode']
 target = 'returns'
 
-
-# Split data
-X = df_encoded[features]
-y = df_encoded[target]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 # Latih model
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
