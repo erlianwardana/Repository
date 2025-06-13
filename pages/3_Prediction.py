9022d00823d21e5251ca6891435c76ee405155d3
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-06-13 19:01:45 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -4,8 +4,4 @@ import streamlit as st
 # Load data
 df = pd.read_csv("SuperStore_Sales_Updated.csv")
 
-# Tambahkan ship_mode sebagai fitur
-features = ['category', 'sub_category', 'segment', 'region', 'ship_mode',
-            'price', 'quantity', 'sales', 'profit', 'payment_mode']
-target = 'returns'
 
