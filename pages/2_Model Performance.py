6573b60adecf55929f6655ff49fb74ed471a0f6e
erlianwardana <erlianwardana@students.unnes.ac.id>
2025-06-13 20:30:31 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
